24e3b45487e25874e028a5b5f2a944e5e892c4a5
Gleb <mail@g1eb.com>
2017-03-03 14:41:26 +0100

    Added default variable declarations and init.

--- src/app/calendar-heatmap.ts
+++ src/app/calendar-heatmap.ts
@@ -11,4 +11,42 @@ export class CalendarHeatmap  {
   @Input() overview: string;
 
   @Output() handler: EventEmitter<object> = new EventEmitter<object>();
+
+  // Defaults
+  private gutter: number;
+  private item_gutter: number;
+  private width: number;
+  private height: number;
+  private item_size: number;
+  private label_padding: number;
+  private max_block_height: number;
+  private transition_duration: number;
+  private in_transition: boolean;
+
+  // Tooltip defaults
+  private tooltip_width: number;
+  private tooltip_padding: number;
+
+  // Overview defaults
+  private history: Array<string>;
+  private selected: object;
+
+  constructor() {
+    this.gutter = 5;
+    this.item_gutter = 1;
+    this.width = 1000;
+    this.height = 200;
+    this.item_size = 10;
+    this.label_padding = 40;
+    this.max_block_height = 20;
+    this.transition_duration = 500;
+    this.in_transition = false;
+
+    this.tooltip_width = 250;
+    this.tooltip_padding = 15;
+
+    this.overview = 'year';
+    this.history = ['year'];
+    this.selected = {};
+  }
 }
