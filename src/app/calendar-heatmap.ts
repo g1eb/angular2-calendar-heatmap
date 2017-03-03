f9a141f3a205379c541a9e0b687312fb3ee638d6
Gleb <mail@g1eb.com>
2017-03-03 15:59:33 +0100

    Initialize svg on ngAfterViewInit callback.

--- src/app/calendar-heatmap.ts
+++ src/app/calendar-heatmap.ts
@@ -1,12 +1,15 @@
 import { Component, Input, Output, EventEmitter, ViewChild } from '@angular/core';
 
+// Declare d3 global variable
+declare var d3:any;
+
 @Component({
   selector: 'calendar-heatmap',
   template: `<div #root class="calendar-heatmap"></div>`,
   styleUrls:  ['./src/app/calendar-heatmap.css'],
 })
 export class CalendarHeatmap  {
-  @ViewChild('root') element: object;
+  @ViewChild('root') element: any;
 
   @Input() data: Array<object>;
   @Input() color: string = '#ff4500';
@@ -32,4 +35,33 @@ export class CalendarHeatmap  {
   // Overview defaults
   private history = ['year'];
   private selected = {};
+
+  // D3 related variables
+  private svg: any;
+  private items: any;
+  private labels: any;
+  private buttons: any;
+  private tooltip: any;
+
+  /**
+   * Get hold of the root element and append our svg
+   */
+  ngAfterViewInit() {
+    var element = this.element.nativeElement;
+
+    // Initialize svg element
+    this.svg = d3.select(element)
+      .append('svg')
+      .attr('class', 'svg');
+
+    // Initialize main svg elements
+    this.items = this.svg.append('g');
+    this.labels = this.svg.append('g');
+    this.buttons = this.svg.append('g');
+
+    // Add tooltip to the same element as main svg
+    this.tooltip = d3.select(element).append('div')
+      .attr('class', 'heatmap-tooltip')
+      .style('opacity', 0);
+  }
 }
