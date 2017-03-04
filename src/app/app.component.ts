9140e023df3e4a0932acb8b7fde6d964a9743984
Gleb <mail@g1eb.com>
2017-03-04 13:50:34 +0100

    Init an array of dummy data in app component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+// Declare global variables
+declare var d3: any;
+declare var moment: any;
+
 @Component({
   selector: 'my-app',
   template: `
@@ -12,12 +16,41 @@ import { Component } from '@angular/core';
   `,
 })
 export class AppComponent  {
-  data = [{
-    'key': 'value',
-  }];
+
+  // Initialize random data for the demo
+  private now = moment().endOf('day').toDate();
+  private year_ago = moment().startOf('day').subtract(1, 'year').toDate();
+  data = d3.time.days(this.year_ago, this.now).map(function (dateElement: any) {
+    return {
+      date: dateElement,
+      details: Array.apply(null, new Array(Math.floor(Math.random() * 15))).map(function(e: number, i: number, arr: any) {
+        return {
+          'name': 'Project ' + Math.floor(Math.random() * 10),
+          'date': function () {
+            var projectDate = new Date(dateElement.getTime());
+            projectDate.setHours(Math.floor(Math.random() * 24))
+            projectDate.setMinutes(Math.floor(Math.random() * 60));
+            return projectDate;
+          }(),
+          'value': 3600 * ((arr.length - i) / 5) + Math.floor(Math.random() * 3600)
+        }
+      }),
+      init: function () {
+        this.total = this.details.reduce(function (prev: number, e: any) {
+          return prev + e.value;
+        }, 0);
+        return this;
+      }
+    }.init();
+  });
+
+  // Set custom color for the calendar heatmap
   color = '#cd2327';
+
+  // Set overview type (choices are year, month and day)
   overview = 'year';
 
+  // Handler function
   print(val: object):void {
     console.log(val);
   }
