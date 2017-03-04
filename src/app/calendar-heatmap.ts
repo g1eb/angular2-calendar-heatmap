db3f7ca9cb2e8d7eba370a51ebb29ec007056536
Gleb <mail@g1eb.com>
2017-03-04 18:19:45 +0100

    Added draw year overview functionality to calendar heatmap component.

--- src/app/calendar-heatmap.ts
+++ src/app/calendar-heatmap.ts
@@ -169,6 +169,294 @@ export class CalendarHeatmap  {
    * Draw year overview
    */
   drawYearOverview() {
+    // Add current overview to the history
+    if ( this.history[this.history.length-1] !== this.overview ) {
+      this.history.push(this.overview);
+    }
+
+    var max_value = d3.max(this.data, function (d: any) {
+      return d.total;
+    });
+    var color = d3.scale.linear()
+      .range(['#ffffff', this.color || '#ff4500'])
+      .domain([-0.15 * max_value, max_value]);
+
+    this.items.selectAll('.item-circle').remove();
+    this.items.selectAll('.item-circle')
+      .data(this.data)
+      .enter()
+      .append('rect')
+      .attr('class', 'item item-circle')
+      .style('opacity', 0)
+      .attr('x', (d: any) => {
+        return this.calcItemX(d) + (this.item_size - this.calcItemSize(d, max_value)) / 2;
+      })
+      .attr('y', (d: any) => {
+        return this.calcItemY(d) + (this.item_size - this.calcItemSize(d, max_value)) / 2;
+      })
+      .attr('rx', (d: any) => {
+        return this.calcItemSize(d, max_value);
+      })
+      .attr('ry', (d: any) => {
+        return this.calcItemSize(d, max_value);
+      })
+      .attr('width', (d: any) => {
+        return this.calcItemSize(d, max_value);
+      })
+      .attr('height', (d: any) => {
+        return this.calcItemSize(d, max_value);
+      })
+      .attr('fill', (d: any) => {
+        return ( d.total > 0 ) ? color(d.total) : 'transparent';
+      })
+      .on('click', (d: any) => {
+        if ( this.in_transition ) { return; }
+
+        // Don't transition if there is no data to show
+        if ( d.total === 0 ) { return; }
+
+        this.in_transition = true;
+
+        // Set selected date to the one clicked on
+        this.selected = d;
+
+        // Hide tooltip
+        this.hideTooltip();
+
+        // Remove all year overview related items and labels
+        this.removeYearOverview();
+
+        // Redraw the chart
+        this.overview = 'day';
+        this.drawChart();
+      })
+      .on('mouseover', (d: any) => {
+        if ( this.in_transition ) { return; }
+
+        // Pulsating animation
+        var circle = d3.select(d3.event.currentTarget);
+        var repeat = () => {
+          circle = circle.transition()
+            .duration(this.transition_duration)
+            .ease('ease-in')
+            .attr('x', (d: any) => {
+              return this.calcItemX(d) - (this.item_size * 1.1 - this.item_size) / 2;
+            })
+            .attr('y', (d: any) => {
+              return this.calcItemY(d) - (this.item_size * 1.1 - this.item_size) / 2;
+            })
+            .attr('width', this.item_size * 1.1)
+            .attr('height', this.item_size * 1.1)
+            .transition()
+            .duration(this.transition_duration)
+            .ease('ease-in')
+            .attr('x', (d: any) => {
+              return this.calcItemX(d) + (this.item_size - this.calcItemSize(d, max_value)) / 2;
+            })
+            .attr('y', (d: any) => {
+              return this.calcItemY(d) + (this.item_size - this.calcItemSize(d, max_value)) / 2;
+            })
+            .attr('width', (d: any) => {
+              return this.calcItemSize(d, max_value);
+            })
+            .attr('height', (d: any) => {
+              return this.calcItemSize(d, max_value);
+            })
+            .each('end', repeat);
+        };
+        repeat();
+
+        // Construct tooltip
+        var tooltip_html = '';
+        tooltip_html += '<div class="header"><strong>' + (d.total ? this.formatTime(d.total) : 'No time') + ' tracked</strong></div>';
+        tooltip_html += '<div>on ' + moment(d.date).format('dddd, MMM Do YYYY') + '</div><br>';
+
+        // Add summary to the tooltip
+        d.summary.filter((d: any) => {
+          tooltip_html += '<div><span><strong>' + d.name + '</strong></span>';
+          tooltip_html += '<span>' + this.formatTime(d.value) + '</span></div>';
+        });
+
+        // Calculate tooltip position
+        var x = this.calcItemX(d) + this.item_size;
+        if ( this.width - x < (this.tooltip_width + this.tooltip_padding * 3) ) {
+          x -= this.tooltip_width + this.tooltip_padding * 2;
+        }
+        var y = this.calcItemY(d) + this.item_size;
+
+        // Show tooltip
+        this.tooltip.html(tooltip_html)
+          .style('left', x + 'px')
+          .style('top', y + 'px')
+          .transition()
+            .duration(this.transition_duration / 2)
+            .ease('ease-in')
+            .style('opacity', 1);
+      })
+      .on('mouseout', () => {
+        if ( this.in_transition ) { return; }
+
+        // Set circle radius back to what it's supposed to be
+        d3.select(d3.event.currentTarget).transition()
+          .duration(this.transition_duration / 2)
+          .ease('ease-in')
+          .attr('x', (d: any) => {
+            return this.calcItemX(d) + (this.item_size - this.calcItemSize(d, max_value)) / 2;
+          })
+          .attr('y', (d: any) => {
+            return this.calcItemY(d) + (this.item_size - this.calcItemSize(d, max_value)) / 2;
+          })
+          .attr('width', (d: any) => {
+            return this.calcItemSize(d, max_value);
+          })
+          .attr('height', (d: any) => {
+            return this.calcItemSize(d, max_value);
+          });
+
+        // Hide tooltip
+        this.hideTooltip();
+      })
+      .transition()
+        .delay( () => {
+          return (Math.cos(Math.PI * Math.random()) + 1) * this.transition_duration;
+        })
+        .duration(() => {
+          return this.transition_duration;
+        })
+        .ease('ease-in')
+        .style('opacity', 1)
+        .call((transition: any, callback: any) => {
+          if ( transition.empty() ) {
+            callback();
+          }
+          var n = 0;
+          transition
+            .each(function() { ++n; })
+            .each('end', function() {
+              if ( !--n ) {
+                callback.apply(this, arguments);
+              }
+            });
+          }, () => {
+            this.in_transition = false;
+          });
+
+    // Add month labels
+    var today = moment().endOf('day');
+    var today_year_ago = moment().startOf('day').subtract(1, 'year');
+    var month_labels = d3.time.months(today_year_ago.startOf('month'), today);
+    var monthScale = d3.scale.linear()
+      .range([0, this.width])
+      .domain([0, month_labels.length]);
+    this.labels.selectAll('.label-month').remove();
+    this.labels.selectAll('.label-month')
+      .data(month_labels)
+      .enter()
+      .append('text')
+      .attr('class', 'label label-month')
+      .attr('font-size', () => {
+        return Math.floor(this.label_padding / 3) + 'px';
+      })
+      .text((d: any) => {
+        return d.toLocaleDateString('en-us', {month: 'short'});
+      })
+      .attr('x', (d: any, i: number) => {
+        return monthScale(i) + (monthScale(i) - monthScale(i-1)) / 2;
+      })
+      .attr('y', this.label_padding / 2)
+      .on('mouseenter', (d: any) => {
+        if ( this.in_transition ) { return; }
+
+        var selected_month = moment(d);
+        this.items.selectAll('.item-circle')
+          .transition()
+          .duration(this.transition_duration)
+          .ease('ease-in')
+          .style('opacity', (d: any) => {
+            return moment(d.date).isSame(selected_month, 'month') ? 1 : 0.1;
+          });
+      })
+      .on('mouseout', () => {
+        if ( this.in_transition ) { return; }
+
+        this.items.selectAll('.item-circle')
+          .transition()
+          .duration(this.transition_duration)
+          .ease('ease-in')
+          .style('opacity', 1);
+      })
+      .on('click', (d: any) => {
+        if ( this.in_transition ) { return; }
+
+        // Check month data
+        var month_data = this.data.filter((e: any) => {
+          return moment(d).startOf('month') <= moment(e.date) && moment(e.date) < moment(d).endOf('month');
+        });
+
+        // Don't transition if there is no data to show
+        if ( !month_data.length ) { return; }
+
+        // Set selected month to the one clicked on
+        this.selected = {date: d};
+
+        this.in_transition = true;
+
+        // Hide tooltip
+        this.hideTooltip();
+
+        // Remove all year overview related items and labels
+        this.removeYearOverview();
+
+        // Redraw the chart
+        this.overview = 'month';
+        this.drawChart();
+      });
+
+    // Add day labels
+    var day_labels = d3.time.days(moment().startOf('week'), moment().endOf('week'));
+    var dayScale = d3.scale.ordinal()
+      .rangeRoundBands([this.label_padding, this.height])
+      .domain(day_labels.map((d: any) => {
+        return moment(d).weekday();
+      }));
+    this.labels.selectAll('.label-day').remove();
+    this.labels.selectAll('.label-day')
+      .data(day_labels)
+      .enter()
+      .append('text')
+      .attr('class', 'label label-day')
+      .attr('x', this.label_padding / 3)
+      .attr('y', (d: any, i: number) => {
+        return dayScale(i) + dayScale.rangeBand() / 1.75;
+      })
+      .style('text-anchor', 'left')
+      .attr('font-size', () => {
+        return Math.floor(this.label_padding / 3) + 'px';
+      })
+      .text((d: any) => {
+        return moment(d).format('dddd')[0];
+      })
+      .on('mouseenter', (d: any) => {
+        if ( this.in_transition ) { return; }
+
+        var selected_day = moment(d);
+        this.items.selectAll('.item-circle')
+          .transition()
+          .duration(this.transition_duration)
+          .ease('ease-in')
+          .style('opacity', (d: any) => {
+            return (moment(d.date).day() === selected_day.day()) ? 1 : 0.1;
+          });
+      })
+      .on('mouseout', () => {
+        if ( this.in_transition ) { return; }
+
+        this.items.selectAll('.item-circle')
+          .transition()
+          .duration(this.transition_duration)
+          .ease('ease-in')
+          .style('opacity', 1);
+      });
   }
 
   /**
