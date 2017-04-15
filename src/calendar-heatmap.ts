42e7cf4a941960b097d8ae89e72cf37ad2512e4a
Gleb <mail@g1eb.com>
2017-04-15 10:51:11 +0200

    Divide project summary into top 5 and the other projects.

--- src/calendar-heatmap.ts
+++ src/calendar-heatmap.ts
@@ -371,11 +371,25 @@ export class CalendarHeatmap  {
         }
         tooltip_html += '<br />';
 
-        // Add summary to the tooltip
-        for ( var i = 0; i < d.summary.length; i++ ) {
-          tooltip_html += '<div><span><strong>' + d.summary[i].name + '</strong></span>';
-          tooltip_html += '<span>' + this.formatTime(d.summary[i].value) + '</span></div>';
-        };
+        if ( d.summary.length <= 5 ) {
+          for ( var i = 0; i < d.summary.length; i++ ) {
+            tooltip_html += '<div><span><strong>' + d.summary[i].name + '</strong></span>';
+            tooltip_html += '<span>' + this.formatTime(d.summary[i].value) + '</span></div>';
+          };
+        } else {
+          for ( var i = 0; i < 5; i++ ) {
+            tooltip_html += '<div><span><strong>' + d.summary[i].name + '</strong></span>';
+            tooltip_html += '<span>' + this.formatTime(d.summary[i].value) + '</span></div>';
+          };
+          tooltip_html += '<br />';
+
+          var other_projects_sum = 0;
+          for ( var i = 5; i < d.summary.length; i++ ) {
+            other_projects_sum =+ d.summary[i].value;
+          };
+          tooltip_html += '<div><span><strong>Other:</strong></span>';
+          tooltip_html += '<span>' + this.formatTime(other_projects_sum) + '</span></div>';
+        }
 
         // Calculate tooltip position
         var x = yearScale(d.date.year()) + this.tooltip_padding;
